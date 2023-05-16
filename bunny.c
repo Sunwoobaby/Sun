16bf76e725fc69dc2aa225512fe7cdf4e2598a80
Sunwoobaby <128569801+Sunwoobaby@users.noreply.github.com>
2023-05-16 06:26:08 +0000

    조건문

---
+++ bunny.c
@@ -0,0 +1,15 @@
+// #include <stdio.h>
+// main() {
+//   int x;
+//   scanf("%d", &x);
+//   if (x >= 90) {
+//     printf("A학점입니다.");
+//   } else if (x >= 80)
+//     printf("B학점입니다.");
+//   else if (x >= 70)
+//     printf("C학점입니다.");
+//   else if (x >= 60)
+//     printf("D학점입니다.");
+//   else
+//     printf("F학점입니다.");
+// }
